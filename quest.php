ac8d7e9f93eafab0e7de898fa63b003a2b9ecedc
senzxc <devdxd084@gmail.com>
2024-11-05 11:37:40 +0700

    create quest

---
+++ quest.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>quest</title>
+</head>
+<body>
+    <h3>welcome to quest! please select the quest below</h3>
+</body>
+</html>
