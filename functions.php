5218f5e6f71b3eb9d14cbb7b0df92f708bb81070
senzxc <devdxd084@gmail.com>
2024-11-05 12:16:27 +0700

    update functions

--- functions.php
+++ functions.php
@@ -16,4 +16,32 @@ function get_coin() {
     return $_SESSION['coin'] ?? 0;
 }
 
+function complete_quest($quest) {
+    if (isset($_SESSION['inventory']) && !empty($_SESSION['inventory'])) {
+        foreach ($_SESSION['inventory'] as $item) {
+            echo "<li>" . $item . "</li>";
+        }
+    } else {
+        echo "<p>no item</p>";
+    }
+    switch ($quest) {
+        case 'ruins' :
+            if ($_SESSION['inventory']['sword'] >= 1) {
+                $_SESSION['inventory']['sword'] -= 1;
+                $_SESSION['coin'] += 10;
+                return "quest 'ruin' completed! you earned 10 coin.";
+            } else {
+                return "not enough swords.";
+            }
+    }
+}
+
+
+
+if (isset($_POST['quest'])) {
+    complete_quest($_POST['quest']);
+}
+
+
+
 ?>
