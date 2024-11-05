c5d7df7f3cc7b141ac434669280769d388602e52
senzxc <devdxd084@gmail.com>
2024-11-05 13:21:30 +0700

    update quest

--- quest.php
+++ quest.php
@@ -1,3 +1,10 @@
+<?php
+    session_start();
+    require 'functions.php';
+
+    initialize_coin();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,7 +13,37 @@
     <title>quest</title>
 </head>
 <body>
-    <h3>welcome to quest! please select the quest below</h3>
+    <h2>welcome to quest! please select the quest below</h2>
+    <form action="" method="post">
+        <button type="submit" name="quest" value="ruins">ruins</button> <h3>reward 10 coin, need sword x1</h3>
+        <button type="submit" name="quest" value="deepsea">deepsea</button> <h3>reward 20 coin, need bow x1</h3>
+        <button type="submit" name="quest" value="celestia">celestia</button> <h3>reward 50 coin, need claymore x1, shield x1</h3>
+    </form>
+    <br>
+    <button onclick="location.href='index.php';">back</button>
+    <br>
+    <br>
+
+    <?php 
+            if (isset($messeg)) {
+                echo "<p>$messeg</p>";
+            }
+        
+        ?>
+
+
+    <p>your item: </p>
+    <?php
+        if (isset($_SESSION['inventory']) && !empty($_SESSION['inventory'])) {
+            foreach ($_SESSION['inventory'] as $item) {
+                echo "<li>" . $item . "</li>";
+            }
+        } else {
+            echo "<p>no item</p>";
+        }
+    
+    
+    ?>
     
 </body>
 </html>
